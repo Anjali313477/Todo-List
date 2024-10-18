8d4ae9f31cf06fe87523c5e3e946c8d81401dcf7
anjali313477 <anjalitadiyal5@outlook.com>
2024-10-18 20:35:07 +0530

    add submitTask function in js file and add hide class in css

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-function CurrentTime() {
+"use strict"
+const tasks=[];
+function currentTime() {
     const newDate = new Date();
-    const dateTimeString = newDate.toLocaleString();
-    document.querySelector('.date').textContent = dateTimeString;
+    const dateTime = newDate.toLocaleString();
+    document.querySelector('.date').textContent = dateTime;
 }
+let interval = setInterval(currentTime, 1000);
 
-let interval = setInterval(CurrentTime,1000)
-
-
- 
+function submitTask() {
+    const taskInput = document.querySelector('#task').value.trim();
+    if (taskInput === "") {
+        swal("Please enter a task!", "", "warning");
+        return false;
+    }   
+    const taskArea = document.querySelector('.task-area').classList.remove('hide')
+    document.querySelector('#tasks').value=taskInput   
+}
